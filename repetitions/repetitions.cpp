921c1fe7af2a80c11c29f72a1a0fc39f60588bca
Esma Koçak <esma.kocaakk20@gmail.com>
2022-03-27 00:50:58 +0300

    .

--- repetitions/repetitions.cpp
+++ repetitions/repetitions.cpp
@@ -1,6 +1,23 @@
+/* You are given a DNA sequence, a string consisting of characters A, C, G, and T. 
+Your task is to find the longest repetition in the sequence. */
+
 #include<iostream>
 using namespace std;
 int main(){
-   
+   string dna;
+   cin>>dna;
+   int counter=0, max_rep=1;
+   char a = 'A';
+   for(char c : dna) {
+       if(c==a) {
+           counter=counter+1;
+           max_rep = max(counter, max_rep);
+       }
+       else {
+        a=c;
+        counter=1;
+       }
+   }
+   cout<<max_rep<<endl;
     return 0;
 }
